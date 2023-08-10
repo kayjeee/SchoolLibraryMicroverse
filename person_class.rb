15a7d8fbda91c1bed043811d21b3d14f7726067f
kayjee <kagiso.killagram@gmail.com>
2023-08-10 10:34:57 +0200

    Applied changes

--- person_class.rb
+++ person_class.rb
@@ -1,8 +1,10 @@
+require_relative 'nameable_class'
+
 class Person < Nameable
   attr_accessor :name, :age
 
   def initialize(id, name, age)
-    super() # Call super to initialize the parent class (Nameable)
+    super()
     @id = id
     @name = name
     @age = age
